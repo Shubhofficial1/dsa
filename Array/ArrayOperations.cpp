ea7f36d0880cfadb14489508d75c9427259d7b26
kumar <shubham.u.kumar@capgemini.com>
2022-06-05 17:06:34 +0530

    Array Questions

---
+++ Array/ArrayOperations.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+
+/* Array Questions
+
+1. Linear Search
+2. Insert an element at a given position
+3. Delete the first occurence of an element 
+4. Largest element in an array (return index)
+5. Second Largest Element in an array 
+6. Check an array is sorted 
+7. Reverse an array
+8. Remove Duplicates from an sorted array
+9. Move Zeroes to the end
+10. Left Rotate an array by one.
+11. Left Rotate an array by "D" places.
+12. Leader in an array
+13. Max difference of arr[j]-arr[i] where j>i
+14. Frequency in an sorted array
+15. Stock buy & sell(part 1 & 2)
+16. Trapping rainwater problem
+17. Maximum consecutive 1's
+18. Maximum subarray sum
+19. Longest Even Odd Subsequence
+20. Maximum Circular Sum Subarray
+21. Majority element
+22. Minimum consecutive flips
+23. Sliding Window technique
+24. Prefix sum Technique
+*/
+
+
+
+int main(){
+    std::cout << "Hello World";
+}
